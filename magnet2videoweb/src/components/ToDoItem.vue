17d046397afac56cecca2262df7bca58a733261f
JackYinpei <2680887830mbb@gmail.com>
2023-06-08 17:42:41 +0800

    rm -rf web page

--- magnet2videoweb/src/components/ToDoItem.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <input type="checkbox" id="todo-item" checked="false"/>
-        <label for="todo-item">My TODO todo-item</label>
-    </div>
-</template>
-<script>
-    export default {};
-</script>
