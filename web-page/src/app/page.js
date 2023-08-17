fbf71bff3095a166a078a7de2b16036f73ff172c
qcy <2680887830@qq.com>
2023-08-17 16:42:20 +0800

    add input bar in index page after user authenticated

--- web-page/src/app/page.js
+++ web-page/src/app/page.js
@@ -2,12 +2,21 @@
 
 
 import { useSession } from 'next-auth/react'
+import styles from './page.module.css'
 export default function Home() {
 
   const {data, status} = useSession()
   console.log(data, "session")
   
   return (
-    <div>haojiahuo</div>
+    <div>
+      <h1>This is Torrent2Video Index Page</h1>
+      {status === "authenticated" && 
+      <div className={styles.inputContainer}>
+        <input type="text" placeholder="Torrent" className={styles.input} required/>
+        <button type="submit" className={styles.button}>Now</button>
+      </div>
+      }
+    </div>
   )
 }
